ebe2c32e6620448a7df33b6198806c2a06bed0a9
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2015-04-12 21:03:17 +0200

    Initial commit

---
+++ app/src/quiz/Quiz.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  // Prepare the 'quiz' module for subsequent registration of controllers and delegates
+  angular.module('quiz', ['ngMaterial']);
+
+})();
---
+++ e2e-tests/scenarios.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* https://github.com/angular/protractor/blob/master/docs/toc.md */
+
+describe('my app', function() {
+
+  browser.get('index.html');
+
+});
